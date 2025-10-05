ce5b099c4b1dafc09fefdf9c393ff76efddb20c8
LeonBorne <brawlgems100@gmail.com>
2025-10-05 11:26:16 +0545

    Added Java script file

---
+++ nav-script.js
@@ -0,0 +1,36 @@
+const NavBar = document.getElementById("nav-container")
+
+    const StudyAbroad = document.getElementById("study-abroad");
+    const HoverMenu = document.getElementById("study-abroad-hover");
+
+    StudyAbroad.addEventListener("mouseover", showMore);
+    HoverMenu.addEventListener("mouseleave", scheduleHide);
+    HoverMenu.addEventListener("mouseover", showMore);
+
+    let hideTimer; // Variable to hold the timeout ID
+
+    /* I changed this ok*/
+
+
+    function showMore() {
+
+      clearTimeout(hideTimer);
+
+      HoverMenu.classList.add("show");
+      StudyAbroad.innerHTML = "Study Abroad <svg xmlns='http://www.w3.org/2000/svg' height='24px' viewBox='0 -960 960 960' width='24px' fill='#000000'><path d='m296-345-56-56 240-240 240 240-56 56-184-183-184 183Z'/></svg>";
+
+    }
+
+    function hideMore() {
+
+      HoverMenu.classList.remove("show");
+
+      StudyAbroad.innerHTML = 'Study Abroad <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M480-345 240-585l56-56 184 183 184-183 56 56-240 240Z"/></svg>'
+
+    }
+
+    function scheduleHide() {
+
+      hideTimer = setTimeout(hideMore, 1000); // Schedule hideMore to run after 2000ms
+
+    }
